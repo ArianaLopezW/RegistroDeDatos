4327738970389db15e835ce69c892c5aa6ccc143
Ariana Lopez Ward <65257225+ArianaLopezW@users.noreply.github.com>
2020-09-11 23:50:18 -0400

    Update v.2

--- Program.cs
+++ Program.cs
@@ -16,42 +16,76 @@ namespace Registro_Official
                 File.WriteAllText(path, header + Environment.NewLine);
             }
             
-            string ID, name, lastName, age, resp;
+            string ID, name, lastName, age, resp, proc;
             Console.WriteLine("REGISTRO");
-            do
+            do {
+            Console.WriteLine("Elija el proceso que desea ejecutar: " + "\n1. Registrar datos" + "\n2. Listado de datos" + "\n3. Busqueda de datos" + "\n4. Salir");
+            proc = Console.ReadLine();
+
+            switch (proc)
             {
-                Console.WriteLine("Introduzca los siguientes datos:");
-                Console.Write("Cedula: ");
-                ID = Console.ReadLine();
-                Console.Write("Nombre: ");
-                name = Console.ReadLine();
-                Console.Write("Apellido: ");
-                lastName = Console.ReadLine();
-                Console.Write("Edad: ");
-                age = Console.ReadLine();
+                case "1":
+                        do
+                        {
+                        Console.WriteLine("Introduzca los siguientes datos:");
+                        Console.Write("Cedula: ");
+                        ID = Console.ReadLine();
+                        Console.Write("Nombre: ");
+                        name = Console.ReadLine();
+                        Console.Write("Apellido: ");
+                        lastName = Console.ReadLine();
+                        Console.Write("Edad: ");
+                        age = Console.ReadLine();
 
-                Console.WriteLine("Desea: guardar (G), continuar (C), o salir (S)");
-                resp = Console.ReadLine();
+                        Console.WriteLine("Desea: guardar (G), continuar (C), o salir (S)");
+                        resp = Console.ReadLine();
 
-                switch(resp)
-                {
-                    case "G":
-                        try{
-                            using(StreamWriter file = new StreamWriter(path, true))
-                            {
-                                file.WriteLine(ID + "," + name + "," + lastName + "," + age);
-                            }
-                        } catch(Exception e) {
-                            Console.Write(e);
+                        switch(resp)
+                        {
+                            case "G":
+                                try{
+                                    using(StreamWriter file = new StreamWriter(path, true))
+                                    {
+                                        file.WriteLine(ID + "," + name + "," + lastName + "," + age);
+                                    }
+                                } catch(Exception e) {
+                                    Console.Write(e);
+                                }
+                                continue;
+                            case "C":
+                                continue;
+                            case "S":
+                                break;
                         }
-                        continue;
-                    case "C":
-                        continue;
-                    case "S":
-                        Environment.Exit(0);
-                        break;
-                }
-            } while (resp != "S");
+                        } while (resp != "S");
+                    break; 
+                
+                case "2":
+                    try{
+                        using(StreamReader reader = new StreamReader(path, true))
+                        {
+                            Console.WriteLine(reader.ReadToEnd());
+                        }
+
+                    } catch (Exception e) {
+                        Console.WriteLine(e);
+                    }
+                    continue; 
+                case "3":
+                    Console.Write("Introduzca el numero de cedula: ");
+                    ID = Console.ReadLine();
+                    var lines = File.ReadAllLines(path);
+                    foreach (string line in lines)
+                    {
+                        if(line.Contains(ID))
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                    Console.WriteLine("");
+                    continue; 
+                }    
+            } while (proc != "4");
         }
     }
 }
